89d4b6da2f594542fb76db79df2b731f3284d6d1
thathayrodrigues <thathayrodrigues@gmail.com>
2025-03-13 02:39:35 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "API está funcionando corretamente!"}
 from fastapi import FastAPI, File, UploadFile
 import cv2
 import numpy as np
