14e09518ffeb6a67360b9436afef740158359122
thathayrodrigues <thathayrodrigues@gmail.com>
2025-03-13 02:57:39 -0300

    Update server.py

--- server.py
+++ server.py
@@ -32,4 +32,5 @@ def detect_face(image):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
